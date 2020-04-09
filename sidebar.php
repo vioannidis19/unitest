96b8d3557e8795fe3f1d0128f01bc3fc9cbb37f0
Vasileios Ioannidis <vasilis.ioannidis4@gmail.com>
2020-04-09 03:37:57 +0300

    basic website layout and sidebar construction

---
+++ sidebar.php
@@ -0,0 +1,38 @@
+<div class="sidebar">
+    <div class="logo">
+        Logo
+        <!--TODO Add logo image file -->
+    </div>
+    <div class="navlist-container">
+        <ul class="navlist">
+            <!-- TODO PHP to change CSS class for active page effect -->
+            <li class="nav-item item-active">
+                <a href="#">
+                    <i class="fas fa-home"></i>
+                    Αρχική Σελίδα
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#">
+                    <i class="fas fa-pencil-alt"></i>
+                    Διαγωνίσματα
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#">
+                    <i class="fas fa-envelope"></i>
+                    Επικοινωνία
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="user-info">
+        <!-- TODO PHP to show user info if logged in -->
+        <div class="user-icon"></div>
+        <div class="user-name">Ευθύμιος Κελέσμητος</div>
+    </div>
+    <div class="login-logout">
+        <a href="#">Σύνδεση/Εγγραφή</a>
+        <!-- TODO PHP to toggle between login or logout link -->
+    </div>
+</div>
